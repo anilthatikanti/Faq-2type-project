32812f215156ffc9ef4c2eb36ca8ea398e07c700
anilthatikanti <thatikantianil@outlook.com>
2022-07-30 17:05:02 +0000

    faq vrsion:2.0

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,56 +1,42 @@
-import {Component} from 'react'
-
 import './index.css'
 
-class FaqItem extends Component {
-  state = {isShow: false}
+const FaqItem = props => {
+  const {FaqDetails, clickToShow} = props
+  const {id, questionText, answerText, isShow} = FaqDetails
 
-  onClickShowing = () => {
-    this.setState(prevState => ({isShow: !prevState.isShow}))
+  const onClickShowing = () => {
+    clickToShow(id)
   }
 
-  render() {
-    const {isShow} = this.state
-    const {FaqDetails} = this.props
-    const {questionText, answerText} = FaqDetails
-    const buttonSelect = isShow ? (
-      <button
-        className="closeAnswer"
-        type="button"
-        onClick={this.onClickShowing}
-      >
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png "
-          alt="minus"
-        />
-      </button>
-    ) : (
-      <button
-        className="openAnswer"
-        type="button"
-        onClick={this.onClickShowing}
-      >
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png "
-          alt="plus"
-        />
-      </button>
-    )
+  const buttonSelect = isShow ? (
+    <button className="closeAnswer" type="button" onClick={onClickShowing}>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png "
+        alt="minus"
+      />
+    </button>
+  ) : (
+    <button className="openAnswer" type="button" onClick={onClickShowing}>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png "
+        alt="plus"
+      />
+    </button>
+  )
 
-    return (
-      <li className="list">
-        <div className="listUpper">
-          <h1>{questionText}</h1>
-          {buttonSelect}
+  return (
+    <li className="list">
+      <div className="listUpper">
+        <h1>{questionText}</h1>
+        {buttonSelect}
+      </div>
+      {isShow && (
+        <div>
+          <hr />
+          <p>{answerText}</p>
         </div>
-        {isShow && (
-          <div>
-            <hr />
-            <p>{answerText}</p>
-          </div>
-        )}
-      </li>
-    )
-  }
+      )}
+    </li>
+  )
 }
 export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,24 +1,49 @@
 // Write your code here.
 // Write your code here.
+import {Component} from 'react'
 
 import FaqItem from '../FaqItem'
 
 import './index.css'
 
-const Faqs = props => {
-  const {faqsList} = props
+class Faqs extends Component {
+  constructor(props) {
+    super(props)
+    const {faqsList} = props
+    this.state = {FaqList: faqsList}
+  }
 
-  return (
-    <div className="container">
-      <div className="card">
-        <h1 className="mainHeading">FAQs</h1>
-        <ul>
-          {faqsList.map(eachFaq => (
-            <FaqItem FaqDetails={eachFaq} key={eachFaq.id} />
-          ))}
-        </ul>
+  clickToShow = id => {
+    this.setState(prevState => ({
+      FaqList: prevState.FaqList.map(eachFaq => {
+        if (eachFaq.id === id) {
+          return {...eachFaq, isShow: !eachFaq.isShow}
+        }
+        return eachFaq
+      }),
+    }))
+  }
+
+  render() {
+    const {FaqList} = this.state
+    console.log(FaqList)
+
+    return (
+      <div className="container">
+        <div className="card">
+          <h1 className="mainHeading">FAQs</h1>
+          <ul>
+            {FaqList.map(eachFaq => (
+              <FaqItem
+                FaqDetails={eachFaq}
+                key={eachFaq.id}
+                clickToShow={this.clickToShow}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 export default Faqs
